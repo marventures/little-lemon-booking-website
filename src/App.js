1cbf79bde8eb7c5ab7ac908e819abb236f4e734f
Marved <108392678+MarvedCodes@users.noreply.github.com>
2023-01-10 00:11:08 +0800

    Add NavBar and Footer components to HomePage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import HomePage from './pages/HomePage';
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-      <NavBar />
       <HomePage />
-      <Footer />
     </>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,14 +2,18 @@ import Hero from '../components/Hero';
 import Specials from '../components/Specials';
 import Testimonials from '../components/Testimonials';
 import About from '../components/About';
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
 
 const HomePage = () => {
   return (
     <main>
+      <NavBar />
       <Hero />
       <Specials />
       <Testimonials />
       <About />
+      <Footer />
     </main>
   );
 };
