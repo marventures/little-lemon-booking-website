645744e729684dac814b7d9d9a82f4268b70d9d9
Marved <108392678+MarvedCodes@users.noreply.github.com>
2023-01-09 19:48:00 +0800

    Create necessary components

---
+++ src/components/BookingPage.js
@@ -0,0 +1,5 @@
+const BookingPage = () => {
+  return <div>BookingPage</div>;
+};
+
+export default BookingPage;
---
+++ src/components/CallToAction.js
@@ -0,0 +1,5 @@
+const CallToAction = () => {
+  return <div>CallToAction</div>;
+};
+
+export default CallToAction;
