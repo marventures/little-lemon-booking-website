37f7785dc651f10051425b9688e197f91ea1c380
Marved <108392678+MarvedCodes@users.noreply.github.com>
2023-01-10 21:37:11 +0800

    Add Specials component styles

--- src/components/Specials/Specials.js
+++ src/components/Specials/Specials.js
@@ -3,7 +3,10 @@ import './Specials.styles.css';
 const Specials = () => {
   return (
     <section name='menu' className='specials'>
-      Specials
+      <div className='specials-header'>
+        <h1 className='specials-title'>This weeks Specials!</h1>
+        <button className='menu-button'>Online Menu</button>
+      </div>
     </section>
   );
 };
