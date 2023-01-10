5da2994b4ed5b59ee01e6088869421520ab8a3ac
Marved <108392678+MarvedCodes@users.noreply.github.com>
2023-01-10 21:12:57 +0800

    Apply hero section styles

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,7 +1,30 @@
 import './Hero.styles.css';
+import restaurantFood from '../../assets/restaurantfood.jpg';
+import { Link as LinkR } from 'react-router-dom';
 
 const Hero = () => {
-  return <section className='hero'>Hero</section>;
+  return (
+    <section className='hero'>
+      <div className='display-container'>
+        <h1 className='display-title'>Little Lemon</h1>
+        <h3 className='display-subtitle'>Chicago</h3>
+        <h4 className='display-text'>
+          We are a family owned Mediterranean restaurant, focused on traditional
+          recipes served with a modern twist.
+        </h4>
+        <LinkR to='/bookings' className='reserve-button'>
+          Reserve a Table
+        </LinkR>
+      </div>
+      <div>
+        <img
+          src={restaurantFood}
+          alt='restaurant food'
+          className='display-image'
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
