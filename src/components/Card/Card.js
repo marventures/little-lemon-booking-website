317369f246fc17fb7008bb12e726ed4602886198
Marved <108392678+MarvedCodes@users.noreply.github.com>
2023-01-11 13:32:32 +0800

    Create Card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,30 @@
+import './Card.styles.css';
+import { specials } from '../../data';
+import { RiEBike2Line } from 'react-icons/ri';
+
+// TODO: CARD COMPONENT
+const Card = () => {
+  const specialties = specials.map(
+    ({ id, image, name, price, description }) => {
+      return (
+        <div key={id} className='card-container'>
+          <img src={image} alt={name} className='specials-image' />
+          <div className='specials-details'>
+            <div className='specials-name'>
+              <p>{name}</p>
+              <p className='specials-price'>{price}</p>
+            </div>
+            <p className='specials-description'>{description}</p>
+            <div className='specials-delivery'>
+              <p className='specials-name'>Order a delivery</p>
+              <RiEBike2Line />
+            </div>
+          </div>
+        </div>
+      );
+    }
+  );
+  return <>{specialties}</>;
+};
+
+export default Card;
