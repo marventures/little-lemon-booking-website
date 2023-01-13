a89a20eb91f915dedaed92b15385eaade771d68a
Marved <108392678+MarvedCodes@users.noreply.github.com>
2023-01-13 19:10:40 +0800

    Change availableTimes using the useReducer

--- src/pages/BookingPage.js
+++ src/pages/BookingPage.js
@@ -1,9 +1,21 @@
+import { useReducer } from 'react';
 import BookingForm from '../components/BookingForm/BookingForm';
 
+const updateTimes = (state, action) => {
+  // For now, the function can return the same available times regardless of the date.
+  return { ...state };
+};
+
+const initializeTimes = () => {
+  return { times: ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'] };
+};
+
 const BookingPage = () => {
+  const [availableTimes, dispatch] = useReducer(updateTimes, initializeTimes());
+
   return (
     <>
-      <BookingForm />
+      <BookingForm availableTimes={availableTimes} dispatch={dispatch} />
     </>
   );
 };
