8c2b4a55b321937caa5fa0734259bdf12beae3d8
Marved <108392678+MarvedCodes@users.noreply.github.com>
2023-01-14 16:02:25 +0800

    Display confirmation details

--- src/components/ConfirmedBooking/ConfirmedBooking.js
+++ src/components/ConfirmedBooking/ConfirmedBooking.js
@@ -1,5 +1,22 @@
+import { useEffect, useState } from 'react';
+
 const ConfirmedBooking = () => {
-  return <div>Booking Confirmed!</div>;
+  const [reservedDetails, setReserveDetails] = useState('');
+
+  useEffect(() => {
+    const data = localStorage.getItem('Bookings');
+    if (data !== null) setReserveDetails(JSON.parse(data));
+  }, []);
+
+  return (
+    <>
+      <h1>Booking Confirmed!</h1>
+      <h2>DATE: {reservedDetails.date}</h2>
+      <h3>TIME: {reservedDetails.time}</h3>
+      <h3>GUESTS: {reservedDetails.guests}</h3>
+      <h3>OCCASION: {reservedDetails.occasion}</h3>
+    </>
+  );
 };
 
 export default ConfirmedBooking;
